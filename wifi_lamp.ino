fdffec6573b747beadc22716f24cd2534cd98800
Doug McInnes <doug@dougmcinnes.com>
2015-03-31 22:00:51 -0700

    update to use ESP8266 module

--- wifi_lamp.ino
+++ wifi_lamp.ino
@@ -1,244 +1,60 @@
-#include <Adafruit_CC3000.h>
-#include <SPI.h>
-#include "utility/debug.h"
-#include "utility/socket.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 #include "LPD8806.h"
 #include "wifi_creds.h"
 
-// These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   2  // MUST be an interrupt pin!
-// These can be any two pins
-#define ADAFRUIT_CC3000_VBAT  9
-#define ADAFRUIT_CC3000_CS    10
-// Use hardware SPI for the remaining pins
-// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+ESP8266WebServer server(80);
 
-Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
-                                         SPI_CLOCK_DIV2); // you can change this clock speed
+#define LED_COUNT 16
 
-#define LISTEN_PORT           80      // What TCP port to listen on for connections.
-                                      // The HTTP protocol uses port 80 by default.
+#define DATA_PIN  0
+#define CLOCK_PIN 2
 
-#define MAX_ACTION            10      // Maximum length of the HTTP action that can be parsed.
+LPD8806 strip = LPD8806(LED_COUNT, DATA_PIN, CLOCK_PIN);
 
-#define MAX_PATH              64      // Maximum length of the HTTP request path that can be parsed.
-                                      // There isn't much memory available so keep this short!
+void handle_root() {
+  server.send(200, "text/plain", "hello from esp8266!");
 
-#define BUFFER_SIZE           MAX_ACTION + MAX_PATH + 20  // Size of buffer for incoming request data.
-                                                          // Since only the first line is parsed this
-                                                          // needs to be as large as the maximum action
-                                                          // and path plus a little for whitespace and
-                                                          // HTTP version.
-
-#define TIMEOUT_MS            500    // Amount of time in milliseconds to wait for
-                                     // an incoming request to finish.  Don't set this
-                                     // too high or your server could be slow to respond.
-
-#define LED_COUNT             16
-#define LED_DATA_PIN          6
-#define LED_CLOCK_PIN         7
-
-LPD8806 strip = LPD8806(LED_COUNT, LED_DATA_PIN, LED_CLOCK_PIN);
-
-Adafruit_CC3000_Server httpServer(LISTEN_PORT);
-uint8_t buffer[BUFFER_SIZE+1];
-int bufindex = 0;
-char action[MAX_ACTION+1];
-char path[MAX_PATH+1];
-
-// Tries to read the IP address and other connection details
-bool displayConnectionDetails(void) {
-  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
-
-  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv)) {
-    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
-    return false;
-  } else {
-    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
-    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
-    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
-    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
-    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
-    Serial.println();
-    return true;
-  }
-}
-
-// Parse the action and path from the first line of an HTTP request.
-void parseFirstLine(char* line, char* action, char* path) {
-  // Parse first word up to whitespace as action.
-  char* lineaction = strtok(line, " ");
-  if (lineaction != NULL)
-    strncpy(action, lineaction, MAX_ACTION);
-  // Parse second word up to whitespace as path.
-  char* linepath = strtok(NULL, " ");
-  if (linepath != NULL)
-    strncpy(path, linepath, MAX_PATH);
-}
-
-// Return true if the buffer contains an HTTP request.  Also returns the request
-// path and action strings if the request was parsed.  This does not attempt to
-// parse any HTTP headers because there really isn't enough memory to process
-// them all.
-// HTTP request looks like:
-//  [method] [path] [version] \r\n
-//  Header_key_1: Header_value_1 \r\n
-//  ...
-//  Header_key_n: Header_value_n \r\n
-//  \r\n
-bool parseRequest(uint8_t* buf, int bufSize, char* action, char* path) {
-  // Check if the request ends with \r\n to signal end of first line.
-  if (bufSize < 2)
-    return false;
-  if (buf[bufSize-2] == '\r' && buf[bufSize-1] == '\n') {
-    parseFirstLine((char*)buf, action, path);
-    return true;
-  }
-  return false;
-}
-
-void monitor(void) {
-  Serial.begin(115200);
-
-  Serial.println(F("Hello, CC3000!\n"));
-
-  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
-
-  while (! displayConnectionDetails()) {
-    delay(1000);
-  }
-}
-
-void connect(void) {
-  strip.setPixelColor(0, 0, 0, 1);
+  strip.setPixelColor(1, 1, 0, 0);
   strip.show();
-
-  if (!cc3000.begin()) {
-    strip.setPixelColor(0, 1, 0, 0);
-    strip.show();
-    // failed to begin
-    while(1);
-  }
-
-  strip.setPixelColor(1, 0, 0, 1);
-  strip.show();
-
-  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
-    strip.setPixelColor(1, 1, 0, 0);
-    strip.show();
-    // failed
-    while(1);
-  }
-
-  strip.setPixelColor(2, 0, 0, 1);
-  strip.show();
-
-  while (!cc3000.checkDHCP())
-  {
-    strip.setPixelColor(2, 0, 0, 0);
-    strip.show();
-    delay(500); // ToDo: Insert a DHCP timeout!
-    strip.setPixelColor(2, 0, 0, 1);
-    strip.show();
-  }
-
-  strip.setPixelColor(3, 0, 0, 1);
-  strip.show();
-
-  // Start listening for connections
-  httpServer.begin();
 }
 
 void setup(void) {
-  // Start up the LED strip
   strip.begin();
 
-  // Update the strip, to start they are all 'off'
   strip.show();
 
-  connect();
-  /* monitor(); */
+  Serial.begin(115200);
+
+  // Connect to WiFi network
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(WLAN_SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handle_root);
+
+  server.on("/inline", [](){
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  strip.setPixelColor(0, 0, 0, 1);
+  strip.show();
 }
 
 void loop(void) {
-  strip.setPixelColor(0, 1, 1, 0);
-  strip.show();
-
-  // Try to get a client which is connected.
-  Adafruit_CC3000_ClientRef client = httpServer.available();
-  if (client) {
-    strip.setPixelColor(0, 0, 1, 0);
-    strip.show();
-    // Process this request until it completes or times out.
-    // Note that this is explicitly limited to handling one request at a time!
-
-    // Clear the incoming data buffer and point to the beginning of it.
-    bufindex = 0;
-    memset(&buffer, 0, sizeof(buffer));
-
-    // Clear action and path strings.
-    memset(&action, 0, sizeof(action));
-    memset(&path,   0, sizeof(path));
-
-    // Set a timeout for reading all the incoming data.
-    unsigned long endtime = millis() + TIMEOUT_MS;
-
-    // Read all the incoming data until it can be parsed or the timeout expires.
-    bool parsed = false;
-    while (!parsed && (millis() < endtime) && (bufindex < BUFFER_SIZE)) {
-      if (client.available()) {
-        buffer[bufindex++] = client.read();
-      }
-      parsed = parseRequest(buffer, bufindex, action, path);
-    }
-
-    strip.setPixelColor(1, 0, 1, 0);
-    strip.show();
-
-    // Handle the request if it was parsed.
-    if (parsed) {
-      // Check the action to see if it was a GET request.
-      if (strcmp(action, "GET") == 0) {
-        strip.setPixelColor(2, 0, 1, 0);
-        strip.show();
-        // Respond with the path that was accessed.
-        // First send the success response code.
-        client.fastrprintln(F("HTTP/1.1 200 OK"));
-        // Then send a few headers to identify the type of data returned and that
-        // the connection will not be held open.
-        client.fastrprintln(F("Content-Type: text/plain"));
-        client.fastrprintln(F("Connection: close"));
-        client.fastrprintln(F("Server: Adafruit CC3000"));
-        // Send an empty line to signal start of body.
-        client.fastrprintln(F(""));
-        // Now send the response data.
-        client.fastrprintln(F("Hello world!"));
-        client.fastrprint(F("You accessed path: ")); client.fastrprintln(path);
-      }
-      else {
-        // Unsupported action, respond with an HTTP 405 method not allowed error.
-        client.fastrprintln(F("HTTP/1.1 405 Method Not Allowed"));
-        client.fastrprintln(F(""));
-      }
-    }
-
-    strip.setPixelColor(3, 0, 1, 0);
-    strip.show();
-
-    // Wait a short period to make sure the response had time to send before
-    // the connection is closed (the CC3000 sends data asyncronously).
-    delay(100);
-
-    // Close the connection when done.
-    client.close();
-  }
-
-  delay(100);
-
-  for (int i=0; i < strip.numPixels(); i++) {
-    strip.setPixelColor(i, 0, 0, 0);
-  }
-  strip.show();
-
-  delay(100);
+  server.handleClient();
 }
