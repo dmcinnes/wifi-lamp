c6794d9d390e8cfe0893139c4c839b44f12fd60a
Doug McInnes <doug@dougmcinnes.com>
2015-04-13 22:23:31 -0700

    remove led code from server

--- esp8266/server.ino
+++ esp8266/server.ino
@@ -1,23 +1,9 @@
-#include <SPI.h>
-#include <LPD8806.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include "wifi_creds.h"
 
-typedef void (*lampAction)(unsigned long);
-lampAction currentLampAction;
-
 ESP8266WebServer server(80);
-
-#define LED_COUNT 16
-
-#define DATA_PIN  2
-#define CLOCK_PIN 0
-
-unsigned long lastMillis;
-
-LPD8806 strip = LPD8806(LED_COUNT, DATA_PIN, CLOCK_PIN);
 
 void OK() {
   server.send(200, "text/plain", "OK");
@@ -29,12 +15,10 @@ void setupServer() {
   });
 
   server.on("/bubble", HTTP_POST, [](){
-    currentLampAction = &bubble;
     OK();
   });
 
   server.on("/rainbow", HTTP_POST, [](){
-    currentLampAction = &rainbow;
     OK();
   });
 }
@@ -45,10 +29,6 @@ void setup(void) {
   // Connect to WiFi network
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   Serial.println("");
-
-  strip.begin();
-
-  strip.show();
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
@@ -65,109 +45,8 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
-
-  strip.setPixelColor(0, 0, 0, 1);
-  strip.show();
-
-  lastMillis = millis();
-  randomSeed(analogRead(0));
-
-  currentLampAction = &bubble;
-}
-
-// Input a value 0 to 384 to get a color value.
-// The colours are a transition r - g -b - back to r
-
-uint32_t wheel(uint16_t WheelPos) {
-  byte r, g, b;
-  switch(WheelPos / 128) {
-    case 0:
-      r = 127 - WheelPos % 128; // Red down
-      g = WheelPos % 128;       // Green up
-      b = 0;                    // blue off
-      break;
-    case 1:
-      g = 127 - WheelPos % 128; // green down
-      b = WheelPos % 128;       // blue up
-      r = 0;                    // red off
-      break;
-    case 2:
-      b = 127 - WheelPos % 128; // blue down
-      r = WheelPos % 128;       // red up
-      g = 0;                    // green off
-      break;
-  }
-  return(strip.Color(r,g,b));
-}
-
-const unsigned int bubbleCount = 5;
-unsigned int bubbleUpdateOn[] = {0, 0, 0, 0, 0};
-unsigned int bubbleTimers[]   = {0, 0, 0, 0, 0};
-unsigned int bubbleLeds[]     = {0, 0, 0, 0, 0};
-void bubble(unsigned long delta) {
-  bool show = false;
-  unsigned int i, led, color;
-
-  for (i = 0; i < bubbleCount; i++) {
-    if (bubbleUpdateOn[i] == 0) {
-      bubbleUpdateOn[i] = random(30, 150);
-    }
-
-    bubbleTimers[i] += delta;
-
-    if (bubbleTimers[i] > bubbleUpdateOn[i]) {
-      bubbleTimers[i] -= bubbleUpdateOn[i];
-      led = bubbleLeds[i];
-      if (led > 0) {
-        strip.setPixelColor(led - 1, 0, 0, 0);
-      }
-      if (led < LED_COUNT) {
-        color = strip.getPixelColor(led) & 0x7f;
-        strip.setPixelColor(led, 0, 0, color + 20);
-        bubbleLeds[i] = led + 1;
-      } else {
-        // reset
-        bubbleLeds[i]     = 0;
-        bubbleUpdateOn[i] = 0;
-        bubbleTimers[i]   = 0;
-      }
-      show = true;
-    }
-  }
-  if (show) {
-    strip.show();
-  }
-}
-
-const unsigned int rainbowDelay = 50;
-unsigned int rainbowOffset  = 0;
-unsigned int rainbowTimeout = 0;
-void rainbow(unsigned long delta) {
-  unsigned int i;
-
-  rainbowTimeout += delta;
-  if (rainbowTimeout > rainbowDelay) {
-    rainbowTimeout -= rainbowDelay;
-    rainbowOffset++;
-    if (rainbowOffset > 384) {
-      rainbowOffset = 0;
-    }
-    for (i=0; i < LED_COUNT; i++) {
-      strip.setPixelColor(i, wheel( (i + rainbowOffset) % 384));
-    }
-    strip.show();
-  }
 }
 
 void loop(void) {
-  unsigned long currentMillis = millis();
-  unsigned long delta = currentMillis - lastMillis;
-  lastMillis = currentMillis;
-
-  (*currentLampAction)(delta);
-
   server.handleClient();
-
-  // let the WIFI stack do its thing
-  yield();
 }
