586494446bc32e4e22efe2cdace189c594c315a1
Doug McInnes <doug@dougmcinnes.com>
2015-05-03 20:05:02 -0700

    add a telnet command

--- commands.lua
+++ commands.lua
@@ -1,3 +1,13 @@
 Server.cmd('restart', function ()
-  node.restart()
+  tmr.alarm(0, 100, 0, function ()
+    node.restart()
+  end)
+end)
+
+Server.cmd('telnet', function ()
+  tmr.alarm(0, 100, 0, function ()
+    -- need to close web server for this
+    Server.server:close()
+    require 'telnet'
+  end)
 end)
